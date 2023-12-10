ee5a585764be2e6aa00bf39e9b1faa86a26f5399
João Pedro Souza Arruda <joao.arruda.jope16@gmail.com>
2023-12-10 15:19:48 -0300

    buttonAppBar

--- page.py
+++ page.py
@@ -9,54 +9,50 @@ from flet import (
     PopupMenuItem,
     colors,
     icons,
-    margin
+    margin,
+    TextButton,
+    ElevatedButton,
+    ButtonStyle,
+    MaterialState,
+    Row,
+    
 )
-# class TrelloApp:
-#     def __init__(self, page: Page):
-#         self.page = page
-#         self.appbar_items = [
-#             PopupMenuItem(text="Login"),
-#             PopupMenuItem(),  # divider
-#             PopupMenuItem(text="Settings")
-#         ]
-#         self.appbar = AppBar(
-#             leading=Icon(icons.GRID_GOLDENRATIO_ROUNDED),
-#             leading_width=100,
-#             title=Text("Trolli",size=32, text_align="start"),
-#             center_title=False,
-#             toolbar_height=75,
-#             bgcolor=colors.LIGHT_BLUE_ACCENT_700,
-#             actions=[
-#                 Container(
-#                     content=PopupMenuButton(
-#                         items=self.appbar_items
-#                     ),
-#                     margin=margin.only(left=50, right=25)
-#                 )
-#             ],
-#         )
-#         self.page.appbar = self.appbar
-#         self.page.update()
 class MainApp:
      def __init__(self, page: Page):
         self.page = page
-        self.appbar_items = [
-            PopupMenuItem(text="Login"),
-            PopupMenuItem(),  # divider
-            PopupMenuItem(text="Settings")
-        ]
         self.appbar = AppBar(
             title=Text("CriptoText.",size=32, text_align="start"),
             center_title=False,
             toolbar_height=75,
-            bgcolor=colors.GREEN_800,
+            bgcolor=colors.GREEN_400,
             actions=[
                 Container(
-                    content=PopupMenuButton(
-                        items=self.appbar_items
-                    ),
-                    margin=margin.only(left=50, right=25)
-                )
+                    Row([ 
+                    ElevatedButton("cifrar",
+                                   height= 25,
+                                   style= ButtonStyle(
+                                   color= colors.GREEN_300,
+                                   bgcolor= colors.WHITE,
+                                   shadow_color= colors.BLACK12
+                                    )
+                                ),
+                    ElevatedButton('decifrar',
+                                   height= 25,
+                                   style= ButtonStyle(
+                                   color= colors.GREEN_300,
+                                   bgcolor= colors.WHITE,
+                                   shadow_color= colors.BLACK12,
+                                   )
+                                ),
+                            ],
+                        alignment=MainAxisAlignment.SPACE_AROUND,
+                        ),
+                    bgcolor=colors.AMBER,
+                    border_radius=20,
+                    height= 40,
+                    width= 200,
+                    
+                        )
             ],
         )
         self.page.appbar = self.appbar
@@ -67,9 +63,9 @@ if __name__ == "__main__":
  
     def main(page: Page):
  
-        page.title = "Flet Trello clone"
+        page.title = "CriptoText"
         page.padding = 0
-        page.bgcolor = colors.BLUE_GREY_200
+        page.bgcolor = colors.WHITE
         app = MainApp(page)
         # page.add(app)
         page.update()
