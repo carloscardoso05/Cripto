296eb9bfc2c0edda53b4027d575477e11f5db904
Carlos Cardoso <cvcardos5@gmail.com>
2023-12-09 23:49:35 -0300

    correção de erro

--- page.py
+++ page.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
         page.padding = 0
         page.bgcolor = colors.BLUE_GREY_200
         app = MainApp(page)
-        page.add(app)
+        # page.add(app)
         page.update()
  
     flet.app(target=main, view=flet.WEB_BROWSER)
