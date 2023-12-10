7df8621df5be590ff967da78080bbc753efefe81
João Pedro Souza Arruda <joao.arruda.jope16@gmail.com>
2023-12-10 16:59:29 -0300

    app bar front

--- page.py
+++ page.py
@@ -15,42 +15,55 @@ from flet import (
     ButtonStyle,
     MaterialState,
     Row,
+    MainAxisAlignment,
+    FontWeight
     
 )
 class MainApp:
      def __init__(self, page: Page):
         self.page = page
         self.appbar = AppBar(
-            title=Text("CriptoText.",size=32, text_align="start"),
+            title=Text(
+                "CriptoText.",
+                size=64,
+                text_align="start",
+                font_family= "MuseoModerno",
+                color="#87E178",
+                weight=FontWeight.BOLD,
+                italic=True,
+                ),
             center_title=False,
-            toolbar_height=75,
-            bgcolor=colors.GREEN_400,
+            toolbar_height=160,
+            bgcolor='#F4F4F4',
             actions=[
                 Container(
                     Row([ 
                     ElevatedButton("cifrar",
-                                   height= 25,
+                                   height= 50,
+                                   width= 165,
                                    style= ButtonStyle(
-                                   color= colors.GREEN_300,
-                                   bgcolor= colors.WHITE,
+                                   color= colors.BLACK,
+                                   bgcolor= "#85C7B3",
                                    shadow_color= colors.BLACK12
                                     )
                                 ),
                     ElevatedButton('decifrar',
-                                   height= 25,
+                                   height= 50,
+                                   width= 165,
                                    style= ButtonStyle(
-                                   color= colors.GREEN_300,
-                                   bgcolor= colors.WHITE,
+                                   color= "#797373",
+                                   bgcolor= colors.TRANSPARENT,
                                    shadow_color= colors.BLACK12,
                                    )
                                 ),
                             ],
                         alignment=MainAxisAlignment.SPACE_AROUND,
                         ),
-                    bgcolor=colors.AMBER,
-                    border_radius=20,
-                    height= 40,
-                    width= 200,
+                    bgcolor="#D9D9D9",
+                    border_radius=40,
+                    height= 76,
+                    width= 385,
+                    margin= 20,
                     
                         )
             ],
@@ -62,10 +75,13 @@ class MainApp:
 if __name__ == "__main__":
  
     def main(page: Page):
- 
+        page.fonts = {
+        "MuseoModerno": "https://fonts.googleapis.com/css2?family=MuseoModerno:wght@500&display=swap"
+    }
+
         page.title = "CriptoText"
         page.padding = 0
-        page.bgcolor = colors.WHITE
+        page.bgcolor = "#688787"
         app = MainApp(page)
         # page.add(app)
         page.update()
