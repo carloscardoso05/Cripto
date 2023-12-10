1f3741a678f85deda33b64cf3ecd3a6bb8741380
Carlos Cardoso <cvcardos5@gmail.com>
2023-12-10 19:36:02 -0300

    colocando fontes

--- componentes.py
+++ componentes.py
@@ -1,6 +1,7 @@
 import flet as ft
 
-def BotaoSelecionarArquivos(page: ft.Page, on_selected = None, extensoes_permitidas:list[str] | None = None):
+
+def BotaoSelecionarArquivos(page: ft.Page, on_selected=None, extensoes_permitidas: list[str] | None = None):
     """Botão que abre um diálogo para selecionar arquivos
 
     Args:
@@ -8,7 +9,7 @@ def BotaoSelecionarArquivos(page: ft.Page, on_selected = None, extensoes_permiti
         on_selected (opcional): Função para ser executada quando o usuário selecionar os arquivos. Recebe como argumento uma lista com os caminhos dos arquivos selecionados ou uma lista vazia se nenhum for selecionado.
         extensoes_permitidas (list[str] | None, opcional): Lista das extensões permitidas, None se todas forem. Padrão é None.
     """
-    
+
     def pick_files_result(e: ft.FilePickerResultEvent):
         caminhos = list(map(lambda f: f.path, e.files)) if e.files else []
         on_selected(caminhos)
@@ -24,4 +25,60 @@ def BotaoSelecionarArquivos(page: ft.Page, on_selected = None, extensoes_permiti
             allow_multiple=True,
             allowed_extensions=extensoes_permitidas
         )
+    )
+
+
+def AppBar():
+    titulo = ft.Text(
+        "CriptoText",
+        size=64,
+        text_align="start",
+        font_family="MuseoModerno",
+        color="#87E178",
+        weight=ft.FontWeight.BOLD,
+        italic=True,
+    )
+
+    def BotaoCifrar():
+        return ft.ElevatedButton(
+            content = ft.Text("Cifrar", font_family="Baumans", size=25),
+            height=50,
+            width=165,
+            style=ft.ButtonStyle(
+                color=ft.colors.BLACK,
+                bgcolor="#85C7B3",
+                shadow_color=ft.colors.BLACK12,
+            ),
+        )
+
+    def BotaoDecifrar():
+        return ft.ElevatedButton(
+            content = ft.Text("Decifrar", font_family="Baumans", size=25),
+            height=50,
+            width=165,
+            style=ft.ButtonStyle(
+                color="#797373",
+                bgcolor=ft.colors.TRANSPARENT,
+                shadow_color=ft.colors.BLACK12,
+            )
+        )
+
+    return ft.AppBar(
+        title=titulo,
+        center_title=False,
+        toolbar_height=160,
+        bgcolor='#F4F4F4',
+        actions=[
+                ft.Container(
+                    ft.Row(
+                        [BotaoCifrar(), BotaoDecifrar()],
+                        alignment=ft.MainAxisAlignment.SPACE_AROUND,
+                    ),
+                    bgcolor="#D9D9D9",
+                    border_radius=40,
+                    height=76,
+                    width=385,
+                    margin=20,
+                )
+        ],
     )
--- page.py
+++ page.py
@@ -1,89 +1,19 @@
-import flet
-from flet import (
-    Container,
-    Icon,
-    Page,
-    Text,
-    AppBar,
-    PopupMenuButton,
-    PopupMenuItem,
-    colors,
-    icons,
-    margin,
-    TextButton,
-    ElevatedButton,
-    ButtonStyle,
-    MaterialState,
-    Row,
-    MainAxisAlignment,
-    FontWeight
-    
-)
-class MainApp:
-     def __init__(self, page: Page):
-        self.page = page
-        self.appbar = AppBar(
-            title=Text(
-                "CriptoText.",
-                size=64,
-                text_align="start",
-                font_family= "MuseoModerno",
-                color="#87E178",
-                weight=FontWeight.BOLD,
-                italic=True,
-                ),
-            center_title=False,
-            toolbar_height=160,
-            bgcolor='#F4F4F4',
-            actions=[
-                Container(
-                    Row([ 
-                    ElevatedButton("cifrar",
-                                   height= 50,
-                                   width= 165,
-                                   style= ButtonStyle(
-                                   color= colors.BLACK,
-                                   bgcolor= "#85C7B3",
-                                   shadow_color= colors.BLACK12
-                                    )
-                                ),
-                    ElevatedButton('decifrar',
-                                   height= 50,
-                                   width= 165,
-                                   style= ButtonStyle(
-                                   color= "#797373",
-                                   bgcolor= colors.TRANSPARENT,
-                                   shadow_color= colors.BLACK12,
-                                   )
-                                ),
-                            ],
-                        alignment=MainAxisAlignment.SPACE_AROUND,
-                        ),
-                    bgcolor="#D9D9D9",
-                    border_radius=40,
-                    height= 76,
-                    width= 385,
-                    margin= 20,
-                    
-                        )
-            ],
-        )
-        self.page.appbar = self.appbar
-        self.page.update()
-         
-    
-if __name__ == "__main__":
- 
-    def main(page: Page):
-        page.fonts = {
-        "MuseoModerno": "https://fonts.googleapis.com/css2?family=MuseoModerno:wght@500&display=swap"
+import flet as ft
+from componentes import *
+
+
+def main(page: ft.Page):
+    page.fonts = {
+        "MuseoModerno": r"fontes\MuseoModerno-Regular.ttf",
+        "Baumans": r"fontes\Baumans-Regular.ttf",
+        "Prompt": r"fontes\Prompt-Regular.ttf"
     }
 
-        page.title = "CriptoText"
-        page.padding = 0
-        page.bgcolor = "#688787"
-        app = MainApp(page)
-        # page.add(app)
-        page.update()
- 
-    flet.app(target=main, view=flet.WEB_BROWSER)
+    page.title = "CriptoText"
+    page.padding = 0
+    page.bgcolor = "#688787"
+    page.appbar = AppBar()
+    page.update()
+
+
+ft.app(target=main)
