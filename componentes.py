66c322b7c9aa87364b83c24237d2ad3c282f5dc1
Carlos Cardoso <cvcardos5@gmail.com>
2023-12-11 10:16:25 -0300

    mudando menu cifrar/decifrar

--- componentes.py
+++ componentes.py
@@ -28,7 +28,7 @@ def BotaoSelecionarArquivos(page: ft.Page, on_selected=None, extensoes_permitida
     )
 
 
-def AppBar():
+def AppBar(on_selected):
     modoCifrar = True
     titulo = ft.Text(
         "CriptoText",
@@ -41,46 +41,136 @@ def AppBar():
     )
 
     botaoCifrar = ft.ElevatedButton(
-            content = ft.Text("Cifrar", font_family="Baumans", size=25),
-            height=50,
-            width=165,
-            bgcolor=ft.colors.TEAL_100,
-            data = True
-        )
+        content=ft.Text("Cifrar", font_family="Baumans", size=25),
+        height=50,
+        width=165,
+        bgcolor=ft.colors.TEAL_100,
+        data=True
+    )
 
     botaoDecifrar = ft.ElevatedButton(
-            content = ft.Text("Decifrar", font_family="Baumans", size=25),
-            height=50,
-            width=165,
-            bgcolor=ft.colors.TRANSPARENT,
-            data = False
-        )
-    
+        content=ft.Text("Decifrar", font_family="Baumans", size=25),
+        height=50,
+        width=165,
+        bgcolor=ft.colors.TRANSPARENT,
+        data=False
+    )
+
     def mudarModo(e: ft.ControlEvent):
         nonlocal modoCifrar
         modoCifrar = e.control.data
         botaoCifrar.bgcolor = ft.colors.TEAL_100 if modoCifrar else ft.colors.TRANSPARENT
         botaoDecifrar.bgcolor = ft.colors.TEAL_100 if not modoCifrar else ft.colors.TRANSPARENT
+        on_selected(modoCifrar)
         e.page.update()
 
-    botaoCifrar.on_click=mudarModo
-    botaoDecifrar.on_click=mudarModo
+    botaoCifrar.on_click = mudarModo
+    botaoDecifrar.on_click = mudarModo
 
     return ft.AppBar(
         title=titulo,
         center_title=False,
         toolbar_height=160,
         actions=[
-                ft.Container(
-                    ft.Row(
-                        [botaoCifrar, botaoDecifrar],
-                        alignment=ft.MainAxisAlignment.SPACE_AROUND,
-                    ),
-                    bgcolor="#D9D9D9",
-                    border_radius=40,
-                    height=76,
-                    width=385,
-                    margin=20,
-                )
+            ft.Container(
+                ft.Row(
+                    [botaoCifrar, botaoDecifrar],
+                    alignment=ft.MainAxisAlignment.SPACE_AROUND,
+                ),
+                bgcolor="#D9D9D9",
+                border_radius=40,
+                height=76,
+                width=385,
+                margin=20,
+            )
         ],
     )
+
+
+def inputFraseSenha():
+    return ft.Container(
+        ft.Column(
+            controls=[
+                ft.Text("Frase Senha", font_family="Prompt",
+                        weight=ft.FontWeight.BOLD, text_align=ft.TextAlign.CENTER, size=25, color=ft.colors.WHITE),
+                ft.TextField(
+                    border_radius=ft.border_radius.all(20),
+                    bgcolor=ft.colors.WHITE,
+                    border_color=ft.colors.TEAL_900,
+                    focused_border_width=3
+                )
+            ]
+        ),
+        padding=ft.padding.all(20),
+        border_radius=25,
+        bgcolor=ft.colors.TEAL_700
+    )
+
+
+def inputMensagem():
+    return ft.Container(
+        ft.Column(
+            controls=[
+                ft.Text("Mensagem de Texto", font_family="Prompt",
+                        weight=ft.FontWeight.BOLD, text_align=ft.TextAlign.CENTER, size=25, color=ft.colors.WHITE),
+                ft.TextField(
+                    border_radius=ft.border_radius.all(20),
+                    multiline=True,
+                    min_lines=5,
+                    bgcolor=ft.colors.WHITE,
+                    border_color=ft.colors.TEAL_900,
+                    focused_border_width=3
+                )
+            ]
+        ),
+        padding=ft.padding.all(20),
+        border_radius=25,
+        bgcolor=ft.colors.TEAL_700
+    )
+
+
+menuCifrar = ft.Container(
+    alignment=ft.alignment.center,
+    content=ft.Column(
+        width=400,
+        controls=[
+            inputFraseSenha(),
+            inputMensagem(),
+            ft.Row(
+                controls=[
+                    ft.ElevatedButton(
+                        content=ft.Text(
+                            "Cifrar",
+                            font_family="Baumans"
+                        )
+                    ),
+                    ft.IconButton(icon=ft.icons.UPLOAD_FILE_OUTLINED)
+                ]
+            ),
+        ]
+    )
+)
+
+
+menuDecifrar = ft.Container(
+    visible=False,
+    alignment=ft.alignment.center,
+    content=ft.Column(
+        width=400,
+        controls=[
+            inputFraseSenha(),
+            inputMensagem(),
+            ft.Row(
+                controls=[
+                    ft.ElevatedButton(
+                        content=ft.Text(
+                            "Decifrar",
+                            font_family="Baumans"
+                        )
+                    ),
+                    ft.IconButton(icon=ft.icons.UPLOAD_FILE_OUTLINED)
+                ]
+            ),
+        ]
+    )
+)
--- page.py
+++ page.py
@@ -1,7 +1,18 @@
 import flet as ft
 from componentes import *
 
+
 def main(page: ft.Page):
+    modoCifrar = True
+    def mudarModo(novoModoCifrar):
+        nonlocal modoCifrar
+        global menuDecifrar
+        global menuCifrar
+        modoCifrar = novoModoCifrar
+        menuCifrar.visible = modoCifrar
+        menuDecifrar.visible = not modoCifrar
+        page.update()
+
     page.fonts = {
         "MuseoModerno": r"fontes\MuseoModerno-Regular.ttf",
         "Baumans": r"fontes\Baumans-Regular.ttf",
@@ -11,7 +22,9 @@ def main(page: ft.Page):
     page.title = "CriptoText"
     page.padding = 0
     page.theme = ft.Theme(color_scheme=ft.ColorScheme(primary=ft.colors.TEAL))
-    page.appbar = AppBar()
+    page.appbar = AppBar(on_selected=mudarModo)
+    page.add(menuCifrar)
+    page.add(menuDecifrar)
     page.update()
 
 
